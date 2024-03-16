bc86939fc3de3a9acb17c6a1c967cb7d7763fe9b
m1ker1n <m1ker1nigor@yandex.ru>
2024-03-17 00:13:37 +0300

    added tucnak/telebot framework

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"go-tg-transcriber/config"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	pref := tele.Settings{
+		Token:  cfg.TelegramApiToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	b.Handle(tele.OnVoice, func(c tele.Context) error {
+		g, _ := errgroup.WithContext(context.Background())
+
+		for _, msg := range []string{".", "..", "...", "transcribed as ass"} {
+			g.Go(func() error {
+				return c.Send(msg)
+			})
+			time.Sleep(time.Second)
+		}
+
+		return g.Wait()
+	})
+
+	b.Start()
 }

19c9e065c6f88e41a09d4911c34b905e490a7086
m1ker1n <m1ker1nigor@yandex.ru>
2024-03-17 05:53:46 +0300

    divided config into 2 parts: TelegramConfig & MinioConfig

--- config/config.go
+++ config/config.go
@@ -3,10 +3,25 @@ package config
 import (
 	"fmt"
 	"github.com/ilyakaznacheev/cleanenv"
+	"time"
 )
 
 type Config struct {
-	TelegramApiToken string `env:"TELEGRAM_API_TOKEN" env-required:"true"`
+	Telegram TelegramConfig `env-prefix:"TELEGRAM_"`
+	Minio    MinioConfig    `env-prefix:"MINIO_"`
+}
+
+type TelegramConfig struct {
+	ApiToken string `env:"API_TOKEN" env-required:"true"`
+}
+
+type MinioConfig struct {
+	Endpoint      string        `env:"ENDPOINT" env-required:"true"`
+	Region        string        `env:"REGION" env-default:"us-east-1"`
+	AccessKey     string        `env:"ACCESS_KEY" env-required:"true"`
+	SecretKey     string        `env:"SECRET_KEY" env-required:"true"`
+	BucketName    string        `env:"BUCKET_NAME" env-required:"true"`
+	UploadTimeout time.Duration `env:"UPLOAD_TIMEOUT" env-default:"10m"`
 }
 
 func MustLoad() Config {
